fd3008b579b62c40d248995907190074e8bbd08d
cenfun <cenfun@gmail.com>
2024-05-23 15:05:43 +0800

    add github icon

--- app/page.jsx
+++ app/page.jsx
@@ -142,7 +142,16 @@ export default function Home() {
 
     return (
         <div className="markdown-body">
-            <h1>Welcome to DevImg</h1>
+            <h1>
+                <div className="inline">
+                    <div className="title">DevImg</div>
+                    <a href="https://github.com/cenfun/devimg" target="_blank" className="github">
+                        <svg viewBox="0 0 16 16" width="30" height="30" pointer-events="none" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/></svg>
+                    </a>
+                </div>
+
+
+            </h1>
             <p>Generating npm or github status for profile README.</p>
             <ul>
                 <li><a href="#npm-downloads">npm downloads</a></li>
@@ -166,7 +175,7 @@ export default function Home() {
                     <li>color=44cc11</li>
                     <li>bg=007ec6</li>
                     <li>label={labelDownloads}</li>
-                    <li>output=svg|json</li>
+                    <li>output=svg or json</li>
                 </ul>
             </details>
 
@@ -219,7 +228,7 @@ export default function Home() {
                     <li>axis=99999</li>
                     <li>bg=ffffff</li>
                     <li>label={labelContributions}</li>
-                    <li>output=svg|json</li>
+                    <li>output=svg or json</li>
                 </ul>
             </details>
 
@@ -259,7 +268,7 @@ export default function Home() {
                     <li>colors=dodgerblue,green,orangered...</li>
                     <li>bg=ffffff</li>
                     <li>label={labelLanguages}</li>
-                    <li>output=svg|json</li>
+                    <li>output=svg or json</li>
                 </ul>
             </details>
 
